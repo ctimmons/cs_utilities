cd2fc9fc0687078f8eddf72b8a5c86a7998cad3e
Chris R. Timmons <chris@crtimmonsinc.com>
2013-01-25 20:39:16 -0600

    Forgot to add the code and unit test files (both named Log.cs).

---
+++ Utilities.Core.Tests/Log.cs
@@ -0,0 +1,78 @@
+﻿/* See UNLICENSE.txt file for license details. */
+
+using System;
+using System.Globalization;
+using System.IO;
+using System.Text.RegularExpressions;
+using NUnit.Framework;
+
+namespace Utilities.Core.UnitTests
+{
+  [TestFixture]
+  public class LogTests
+  {
+    private static readonly String _logFileDirectory = FileUtils.GetTemporarySubfolder();
+
+    [SetUp]
+    public void Init()
+    {
+      Directory.CreateDirectory(_logFileDirectory);
+    }
+
+    [TearDown]
+    public void Cleanup()
+    {
+      if (Directory.Exists(_logFileDirectory))
+        Directory.Delete(_logFileDirectory, true /* Delete all files and subdirectories also. */);
+    }
+
+    [Test]
+    public void WriteLineTest()
+    {
+      var logFilename = Path.Combine(_logFileDirectory, "logtest.txt");
+      using (var sw = new StreamWriter(logFilename))
+      {
+        var log = new Log(sw);
+        log.WriteLine(LogEntryType.Info, "info log entry");
+        log.WriteLine(LogEntryType.Warning, "warning log entry");
+        log.WriteLine(LogEntryType.Error, "error log entry");
+      }
+
+      var logFileContents = File.ReadAllLines(logFilename);
+
+      /*
+        Log entries look like this:
+
+          2013-01-26T02:00:26.3637578Z - INF - Hello, world!
+          2013-01-26T02:00:26.3793828Z - WRN - Hello, world!
+          2013-01-26T02:00:26.3793828Z - ERR - Hello, world!
+      */
+
+      Assert.IsTrue(this.DoesMatchLogEntryFormat(logFileContents[0], "INF", "info log entry"), logFileContents[0]);
+      Assert.IsTrue(this.DoesMatchLogEntryFormat(logFileContents[1], "WRN", "warning log entry"), logFileContents[1]);
+      Assert.IsTrue(this.DoesMatchLogEntryFormat(logFileContents[2], "ERR", "error log entry"), logFileContents[2]);
+    }
+
+    private Boolean DoesMatchLogEntryFormat(String logEntry, String logEntryType, String logMessage)
+    {
+      var logEntryParts = logEntry.Split(new[] { " - " }, StringSplitOptions.RemoveEmptyEntries);
+
+      if (logEntryParts.Length != 3)
+        throw new ArgumentException(String.Format("There are {0} parts in logEntry.  There should be 3.  logEntry = '{1}'.", logEntryParts.Length, logEntry));
+
+      /* The timestamp portion should be in the "o" (round trip) format. */
+      try
+      {
+        DateTime.ParseExact(logEntryParts[0], "o", CultureInfo.InvariantCulture);
+      }
+      catch
+      {
+        return false;
+      }
+
+      return
+        logEntryType.Equals(logEntryParts[1], StringComparison.CurrentCulture) &&
+        logMessage.Equals(logEntryParts[2], StringComparison.CurrentCulture);
+    }
+  }
+}
---
+++ Utilities.Core/Log.cs
@@ -0,0 +1,74 @@
+﻿/* See UNLICENSE.txt file for license details. */
+
+using System;
+using System.IO;
+
+namespace Utilities.Core
+{
+  public enum LogEntryType { Info, Warning, Error }
+
+  /* The Log class provides a very simple logging facility.  It's only intended
+     to be used when larger and more complete logging libraries,
+     like Microsoft's Enterprise Library (http://msdn.microsoft.com/en-us/library/ff648951.aspx),
+     are overkill.
+
+     NOTE: Any TextWriter descendent passed to the Log constructor will
+           NOT be closed or disposed of when the instance of Log is disposed.
+  
+     Examples:
+
+       // Writing log data to a file.
+       using (var sw = new StreamWriter("my_log_file.txt"))
+       {
+         var log = new Log(sw);
+         log.WriteLine(LogEntryType.Info, "Hello, world!");
+       }
+
+       // Writing log data to the console.
+       var log = new Log(Console.Out);
+       log.WriteLine(LogEntryType.Info, "Hello, world!");
+  */
+
+  public class Log
+  {
+    private TextWriter _writer;
+
+    private Log()
+      : base()
+    {
+    }
+
+    public Log(TextWriter writer)
+      : this()
+    {
+      writer.CheckForNull("writer");
+      this._writer = writer;
+    }
+
+    public void WriteLine(LogEntryType logEntryType, String message)
+    {
+      /* Timestamps are represented in the Round Trip Format Specifier
+         (http://msdn.microsoft.com/en-us/library/az4se3k1.aspx#Roundtrip). */
+      var timestamp = DateTime.Now.ToUniversalTime().ToString("o");
+
+      String type;
+      switch (logEntryType)
+      {
+        case LogEntryType.Info:
+          type = "INF";
+          break;
+        case LogEntryType.Warning:
+          type = "WRN";
+          break;
+        case LogEntryType.Error:
+          type = "ERR";
+          break;
+        default:
+          type = "UNK";
+          break;
+      }
+
+      this._writer.WriteLine(String.Format("{0} - {1} - {2}", timestamp, type, message));
+    }
+  }
+}
