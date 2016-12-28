f26e9097b2ef32b264a258a6eb8374b308d3058f
ctimmons <chris@crtimmonsinc.com>
2016-12-27 20:22:14 -0600

    Updated exception handling utility.  Removed unlicense.txt.

--- Utilities.Core/Exceptions.cs
+++ Utilities.Core/Exceptions.cs
@@ -2,6 +2,9 @@
 
 using System;
 using System.Collections;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
 using System.Web;
 
 namespace Utilities.Core
@@ -67,32 +70,46 @@ namespace Utilities.Core
        whatever's stored in the Data property, and returns it all as a string.
     
        Useful for logging exception data. */
+
     public static String GetAllExceptionMessages(this Exception ex)
     {
-      if (ex == null)
-      {
-        return "";
-      }
-      else
-      {
-        var nl = Environment.NewLine;
-        var context = HttpContext.Current;
-        var result = ex.Message + nl;
+      var result = new StringBuilder();
+      var context = HttpContext.Current;
+      var nl = Environment.NewLine;
 
-        /* When a file is missing in an ASP.Net app, the error message 'File does not exist' is less than helpful.
-           Find the missing file name and add it to the error message. */
-        if ((context != null) && (result.IndexOf(Properties.Resources.Exceptions_File_Does_Not_Exist, StringComparison.CurrentCultureIgnoreCase) > -1))
-          result += String.Format(Properties.Resources.Exceptions_MissingFile, nl, context.Request.CurrentExecutionFilePath);
+      Action<Exception> rec = null; /* Initially set to null so the lambda can be called recursively. */
+      rec =
+        (currentException) =>
+        {
+          if (currentException == null)
+            return;
 
-        foreach (DictionaryEntry de in ex.Data)
-          result += String.Concat("  ", de.Key, ": ", de.Value, nl);
+          result.AppendLine(currentException.Message);
 
-        /* StackTrace might be null when running this code in NUnit. */
-        if (ex.StackTrace != null)
-          result += String.Format(Properties.Resources.Exceptions_StackTrace, nl, ex.StackTrace.ToString());
+          /* When a file is missing in an ASP.Net app, the error message 'File does not exist' is less than helpful.
+             Find the missing file name and add it to the error message. */
+          if ((context != null) && currentException.Message.ContainsCI(Properties.Resources.Exceptions_File_Does_Not_Exist))
+            result.AppendLineFormat(Properties.Resources.Exceptions_MissingFile, nl, context.Request.CurrentExecutionFilePath);
 
-        return (String.Concat(result, nl, ex.InnerException.GetAllExceptionMessages())).Trim();
-      }
+          foreach (DictionaryEntry de in currentException.Data)
+            result.AppendLine(String.Concat("  ", de.Key, ": ", de.Value));
+
+          if (currentException is SqlException)
+          {
+            foreach (var error in ((SqlException) currentException).Errors.Cast<SqlError>())
+              result.AppendLineFormat("{0}.{1}.{2}({3}): {4} - {5} (state: {6})",
+                error.Source, error.Server, error.Procedure, error.LineNumber, error.Number, error.Message, error.State);
+          }
+
+          /* StackTrace might be null when running this code in NUnit. */
+          if (currentException.StackTrace != null)
+            result.AppendLineFormat(Properties.Resources.Exceptions_StackTrace, nl, currentException.StackTrace.ToString());
+
+          rec(currentException.InnerException);
+        };
+
+      rec(ex);
+      return result.ToString();
     }
   }
 }
